d3ba9534804048c2f99d5e7e1b147dd6ef7edcc5
juan-rybczinski <jhreplay.lee@gmail.com>
2023-07-23 16:59:35 +0900

    Add ResponsiveScrollableCard

--- lib/wirtwirt_common.dart
+++ lib/wirtwirt_common.dart
@@ -5,3 +5,4 @@ export 'src/layout/responsive_center.dart';
 export 'src/layout/responsive_two_column_layout.dart';
 export 'src/component/primary_button.dart';
 export 'src/component/alert_dialogs.dart';
+export 'src/component/responsive_scrollable_card.dart';
